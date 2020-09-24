f32b05ebc2e7033cff44cb99ffaa055f09011a9a
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-09-24 03:47:24 +0300

    Bump version: 0.1.0 → 0.1.1

--- mdformat_black/__init__.py
+++ mdformat_black/__init__.py
@@ -1,6 +1,6 @@
 import black
 
-__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 
 def format_python(unformatted: str, _info_str: str) -> str:
